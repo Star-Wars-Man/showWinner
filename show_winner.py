765bcf7bf7d52271b583537f22ae7c5b44a622c5
Star-Wars-Man <jacob@olsenweb.net>
2024-10-29 09:02:34 -0600

    Create

---
+++ show_winner.py
@@ -0,0 +1,20 @@
+def timeline():
+    print("Starting timeline game")
+    showGameMenu()
+    setUpNewGame()
+    play()
+    showWinner()
+
+def showGameMenu():
+    print("Game Menu")
+
+def setUpNewGame():
+    print("New Game")
+
+def play():
+    print("Playing")
+
+def showWinner():
+    print("Your the Winner")
+
+timeline()
