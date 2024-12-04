81cbb5197b42d3b492f25b72a4c321cbc2c9fd40
Studidart <26combsrobc@washk12.org>
2024-12-04 08:57:11 -0700

    should be done now

--- show_winner.py
+++ show_winner.py
@@ -7,23 +7,54 @@ def timeline():
 
 def showGameMenu():
     print("Game Menu")
-    number_of_players=int(input("how many players? max of 8: "))
+    number_of_players=input("how many players? max of 8: ")
     bots_on=int(input("Bots? if so how many?: "))
+    bots_on=False
+    bot_difficulty=10
     return number_of_players
-def setUpNewGame():
-    print("New Game")
+
+import json
+def shuffle_cards(deck):
+    # TODO, really shuffle it
+    return deck
+
+def setUpNewGame(number_of_players):
+    deck=[]
+    print("loading game cards...")
+    with open('cards.json')as f:
+        deck = json.load(f)
+    print(f"setting up game for {number_of_players} players")
+    starting_player=0
+    shuffled_deck=shuffle_cards(deck)
+    player_hands=[]
+    for i in range(number_of_players):
+        player_hand=[shuffled_deck.pop()]
+        player_hands.append(player_hand)
+
+    return{
+        "player_turn": starting_player,
+        "player_hands": player_hands,
+        "deck":shuffled_deck
+    }
 
 
-def play():
-    print("Playing")
+def play(game_settings, starting_game_table):
+    #current_player=1
+    #while not game_over():
+        #take_turn(current_player)
+        #current_player=pick_next_player()
+    player_hands=starting_game_table["player_hands"]
 
-    # 4 show Winner- Rob Combs, Jacob Olsen 
-        # state:
-            # new: [none]
-            # modified: [end the game] game
-            # required: [players(1)], [points(3)]
 
-def showWinner():
-    print("Your the Winner")
+def showWinner(final_hands):
+    winning_player=0
+    winning_player_score=0
+    for i, hand in enumerate(final_hands):
+        print(f" player {i} had {len(hand)} cards!")
+        if len(hand)>winning_player_score:
+            winning_player=i
+            winning_player_score=len(hand)
+    print(f"player {winning_player} won!!")
+
 
 timeline()
